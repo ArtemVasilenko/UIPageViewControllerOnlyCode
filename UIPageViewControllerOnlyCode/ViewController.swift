cb87f21676a8535619bd41807e0ea3d6eff59ed9
ArtemVasilenko <artemvasilenko88@gmail.com>
2019-03-28 20:52:46 +0200

    woohoo

--- UIPageViewControllerOnlyCode/ViewController.swift
+++ UIPageViewControllerOnlyCode/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var dots: UIPageControl!
     var pageContainer: UIPageViewController!
     var myVCs = [UIViewController] ()
+    var index = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,74 +34,76 @@ class ViewController: UIViewController {
         dots.currentPage = 0
     }
     
-    func createVCs() {
-        for i in 5...10 {
-            let myVC = MyViewController()
+    @IBAction func actionDots(_ sender: UIPageControl) {
+        if index < sender.currentPage {
+            pageContainer.setViewControllers([myVCs[sender.currentPage]], direction: .forward, animated: true, completion: nil)
             
-            let btn = UIButton()
-            btn.backgroundColor = .rnd()
-            btn.frame = CGRect(x: 110, y: 250, width: 100, height: 50)
-            btn.setTitle(String(i), for: .normal)
-            btn.setTitle(String(i + 1), for: .highlighted)
-            btn.setTitleColor(.rnd(), for: .normal)
-            btn.setTitleColor(.rnd(), for: .highlighted)
-            btn.layer.borderWidth = 5
-            btn.layer.borderColor = UIColor.rnd().cgColor
-            btn.layer.cornerRadius = 10
-            
-            myVC.view.backgroundColor = .rnd()
-            myVC.view.addSubview(btn)
-            
-            myVCs.append(myVC)
+        } else {
+            pageContainer.setViewControllers([myVCs[sender.currentPage]], direction: .reverse, animated: true, completion: nil)
+        }
+        
+        index = sender.currentPage
+    }
+        
+        
+        func createVCs() {
+            for i in 5...10 {
+                let myVC = MyViewController()
+                
+                let btn = UIButton()
+                btn.backgroundColor = .rnd()
+                btn.frame = CGRect(x: 110, y: 250, width: 100, height: 50)
+                btn.setTitle(String(i), for: .normal)
+                btn.setTitle(String(i + 1), for: .highlighted)
+                btn.setTitleColor(.rnd(), for: .normal)
+                btn.setTitleColor(.rnd(), for: .highlighted)
+                btn.layer.borderWidth = 5
+                btn.layer.borderColor = UIColor.rnd().cgColor
+                btn.layer.cornerRadius = 10
+                
+                myVC.view.backgroundColor = .rnd()
+                myVC.view.addSubview(btn)
+                
+                myVCs.append(myVC)
+            }
         }
     }
-}
-
-extension UIColor {
     
-    class func rnd() -> UIColor {
+    extension UIColor {
         
-        return UIColor(displayP3Red: .random(in: 0...1), green: .random(in: 0...1), blue: .random(in: 0...1), alpha: .random(in: 0.1...1))
-        
-    }
-}
-
-
-extension ViewController: UIPageViewControllerDelegate, UIPageViewControllerDataSource {
-    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
-        
-        guard let currentIndex = myVCs.firstIndex(of: viewController) else { return nil }
-        
-        let previusIndex = currentIndex - 1
-        
-        guard previusIndex >= 0 else { return nil }
-        guard myVCs.count > previusIndex else { return nil }
-        return myVCs[previusIndex]
-        
-    }
-    
-    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
-        
-        guard let currentIndex = myVCs.firstIndex(of: viewController) else { return nil }
-        
-        let nextIndex = currentIndex + 1
-        
-        guard nextIndex >= 0 else { return nil }
-        guard myVCs.count > nextIndex else { return nil }
-        return myVCs[nextIndex]
+        class func rnd() -> UIColor {
+            
+            return UIColor(displayP3Red: .random(in: 0...1), green: .random(in: 0...1), blue: .random(in: 0...1), alpha: .random(in: 0.1...1))
+            
+        }
     }
     
     
-    func presentationCount(for pageViewController: UIPageViewController) -> Int {
-       return myVCs.count
-    }
-
-    func presentationIndex(for pageViewController: UIPageViewController) -> Int {
-        guard let firstVC = myVCs.first,
-            let vcIndex = self.myVCs.firstIndex(of: firstVC)
-            else { return 0 }
+    extension ViewController: UIPageViewControllerDelegate, UIPageViewControllerDataSource {
+        func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+            
+            guard let currentIndex = myVCs.firstIndex(of: viewController) else { return nil }
+            
+            let previusIndex = currentIndex - 1
+            
+            guard previusIndex >= 0 else { return nil }
+            guard myVCs.count > previusIndex else { return nil }
+            dots.currentPage = previusIndex
+            return myVCs[previusIndex]
+            
+        }
         
-        return vcIndex
-    }
-    
+        func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+            
+            guard let currentIndex = myVCs.firstIndex(of: viewController) else { return nil }
+            
+            let nextIndex = currentIndex + 1
+            
+            guard nextIndex >= 0 else { return nil }
+            guard myVCs.count > nextIndex else { return nil }
+            dots.currentPage = nextIndex
+            return myVCs[nextIndex]
+        }
+        
+        
 }
